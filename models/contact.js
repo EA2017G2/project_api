e368e91abca10ef4317c53090d81150e0960795f
xavo95 <xavier.perarnau.font@estudiant.upc.edu>
2018-01-06 23:57:54 +0100

    Fix for travis
    Signed-off-by: xavo95 <xavier.perarnau.font@estudiant.upc.edu>

--- models/contact.js
+++ models/contact.js
@@ -4,8 +4,8 @@ var mongoose = require('mongoose');
 mongoose.Promise = require('bluebird');
 
 var ContactSchema = new mongoose.Schema({
-    toUserId: { type: Schema.Types.ObjectId, ref: 'User' },
-    fromUserId: { type: Schema.Types.ObjectId, ref: 'User' },
+    toUserId: { type: mongoose.Schema.Types.ObjectId, ref: 'User' },
+    fromUserId: { type: mongoose.Schema.Types.ObjectId, ref: 'User' },
     comment: String,
     signupDate: { type: Date, default: Date.now() }
 });
