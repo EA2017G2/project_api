d2abe922e463bbcab5e6926980289b27514f5d3e
xavo95 <xavier.perarnau.font@estudiant.upc.edu>
2018-01-07 01:08:28 +0100

    Websocket token auth and message protocol
    Signed-off-by: xavo95 <xavier.perarnau.font@estudiant.upc.edu>

--- ws.js
+++ ws.js
@@ -2,31 +2,58 @@
 
 // Load environment variables defined at '.env' file
 var conf = require('./conf/conf');
+var jwt = require('jwt-simple');
 const WebSocket = require('ws');
 
 
+var service = require('./service/index');
 var logger = require('./routes/utils/loggerfactory');
 
 // Start websocket server
-var wss = new WebSocket.Server({ port: conf.getWSPort() });
+var wss = new WebSocket.Server({
+    port: conf.getWSPort(),
+    verifyClient: function (info, cb) {
+        var token = info.req.headers.token;
+        token = "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiI1YTUxNTJmNzkyZmZiZjI2NTAzZTI2YjkiLCJpYXQiOjE1MTUyNzkwOTYsImV4cCI6MTUxNTM2NTQ5NiwibmFtZSI6IjJhIiwiZW1haWwiOiJhM0BhLmNvbSJ9.qMpDg9e-2rby_kOQdS-GBQ1YsrBXyhRvUVh3lyTPgtw";
+        if (!token)
+            cb(false, 401, 'Unauthorized');
+        else {
+            service.decodeToken(token).then(function (response) {
+                info.req.user = response;
+                cb(true)
+            }).catch(function (response) {
+                cb(false, response.status, response.message);
+            });
+        }
+    }
+});
 
-// Broadcast to all.
-wss.broadcast = function broadcast(data) {
-    wss.clients.forEach(function each(client) {
-        if (client.readyState === WebSocket.OPEN) {
-            client.send(data);
+function heartbeat() {
+    this.isAlive = true;
+}
+
+wss.on('connection', function connection(ws, req) {
+    ws.isAlive = true;
+    ws.on('pong', heartbeat);
+
+    ws.on('message', function incoming(message) {
+        var parts = message.split(':');
+        if(parts.length !== 3) {
+            ws.send('Format error');
+        } else {
+            console.log('received: %s from %s', message, req.user.sub);
+            ws.send(req.user.sub);
         }
     });
-};
 
-wss.on('connection', function connection(ws) {
-    logger.log('info', "WebSocket Server connection on: " + portWS, 'app.js', 'root');
-    ws.on('message', function incoming(data) {
-        // Broadcast to everyone else.
-        wss.clients.forEach(function each(client) {
-            if (client !== ws && client.readyState === WebSocket.OPEN) {
-                client.send(data);
-            }
-        });
-    });
+    ws.send('Connected!!!');
 });
+
+const interval = setInterval(function ping() {
+    wss.clients.forEach(function each(ws) {
+        if (ws.isAlive === false) return ws.terminate();
+
+        ws.isAlive = false;
+        ws.ping('', false, true);
+    });
+}, 30000);
