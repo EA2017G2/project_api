ad781ebc4f474f26d4ce6bf757456304f5c17840
xarilo <xavier.rins@gmail.com>
2018-01-20 19:57:47 +0100

    random with filter orientation issue solved

--- controllers/play.js
+++ controllers/play.js
@@ -22,30 +22,57 @@ function getRandom(req,res) {
     } );
 
     console.log(userName);
-    User.find({sex: orientation}, function (err, users) {
-                if (err) return res.status(500).send({message: 'Error al realizar la peticion: ' + err});
-                else if (!users) return res.status(404).send({message: 'No existen usuarios'});
-                else {
-                    for (var i = 0; i < users.length; i++) {
-                        users[i].password = "";
-                        users[i].salt = "";
-                        list.push(users[i].name);
-                    }
-                    var index = list.indexOf(userName);
-                    if (index !== -1) {
-                        list.splice(index,1);
-                    }
-                    console.log('list sin sofia:',list);
+    if(orientation=="both"){
 
-                    for (var j = 0; j < list.length; j++) {
-                       aleatorio = list[Math.floor(Math.random() * list.length)];
+        User.find({}, function (err, users) {
+        if (err) return res.status(500).send({message: 'Error al realizar la peticion: ' + err});
+        else if (!users) return res.status(404).send({message: 'No existen usuarios'});
+        else {
+            for (var i = 0; i < users.length; i++) {
+                users[i].password = "";
+                users[i].salt = "";
+                list.push(users[i].name);
+            }
+            var index = list.indexOf(userName);
+            if (index !== -1) {
+                list.splice(index,1);
+            }
+            console.log('list sin sofia:',list);
 
-                    }
-                    // console.log(list);
-                    res.json(aleatorio);
+            for (var j = 0; j < list.length; j++) {
+                aleatorio = list[Math.floor(Math.random() * list.length)];
+
+            }
+            // console.log(list);
+            res.json(aleatorio);
+        }
+    });}
+    else {
+
+        User.find({sex: orientation}, function (err, users) {
+            if (err) return res.status(500).send({message: 'Error al realizar la peticion: ' + err});
+            else if (!users) return res.status(404).send({message: 'No existen usuarios'});
+            else {
+                for (var i = 0; i < users.length; i++) {
+                    users[i].password = "";
+                    users[i].salt = "";
+                    list.push(users[i].name);
                 }
-            });
+                var index = list.indexOf(userName);
+                if (index !== -1) {
+                    list.splice(index, 1);
+                }
+                console.log('list sin sofia:', list);
 
+                for (var j = 0; j < list.length; j++) {
+                    aleatorio = list[Math.floor(Math.random() * list.length)];
+
+                }
+                // console.log(list);
+                res.json(aleatorio);
+            }
+        });
+    }
 
 
 
